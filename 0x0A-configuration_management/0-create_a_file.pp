08edc69a350277cead949a135644f850266655eb
muhammd2refaat <mohammed_refat@inbox.ru>
2024-06-29 03:14:29 +0300

    chore: Update Puppet manifest to set correct permissions for /tmp/school file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# Create a file with content, owner, group, and mode
+
 file { '/tmp/school':
   content => 'I love Puppet',
-  mode    => '0774',
   owner   => 'www-data',
   group   => 'www-data',
+  mode    => '0774',
 }
