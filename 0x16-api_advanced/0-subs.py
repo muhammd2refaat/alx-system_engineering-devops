68acc7cbb4292647d9025fd8c0b22b0a57c79e69
muhammd2refaat <mohammed_refat@inbox.ru>
2024-08-14 05:38:09 +0300

    feat: Refactor number_of_subscribers function to use requests.get from the requests module

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """
-the number of subscribers (not active users, total subscribers)
-for a given subreddit
+number of subscribers for a given subreddit
 """
-import requests
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-        GET /r/subreddit/aboutread
-            Return information about the subreddit.
-            Data includes the subscriber count, description, and header image.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom"}
-    response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
